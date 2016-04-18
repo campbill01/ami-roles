7e83133d4837f4581f76553f8ec90ae4df1614d3
Bill Campbell <bcampbell@kenzan.com>
2016-04-18 14:43:34 -0400

    update doc

--- jenkins_dsl/ami.groovy
+++ jenkins_dsl/ami.groovy
@@ -2,7 +2,7 @@
 //
 // Amazon Linux AMI ID  http://aws.amazon.com/amazon-linux-ami/
 // HVM (SSD) EBS-Backed 64-bit --  Region: US East N. Virginia
-// Ubuntu 15.10 wily - https://cloud-images.ubuntu.com/locator/ec2/
+// Ubuntu 14.04 LTS  - https://cloud-images.ubuntu.com/locator/ec2/
 // HVM (SSD) EBS-Backed 64-bit --  Region: US East N. Virginia
 def ubuntu_ami = "ami-fce3c696"
 def amazon_ami = "ami-60b6c60a"
